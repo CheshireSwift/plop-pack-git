2bcac54616ad5797caac1b430a64cf5683d7f1d4
Sam Collard <swift@swifteagle.co.uk>
2018-03-20 22:55:14 +0000

    Include actions by default

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { execSync } = require('child_process');
 
 module.exports = function(plop, config) {
+  plop.setDefaultInclude({ actionTypes: true });
+
   plop.setActionType('gitAdd', (answers, config, _plop) => {
     const renderedFiles = config.files.map(file => _plop.renderString(file, answers));
     execSync('git add ' + renderedFiles.join(' '));
